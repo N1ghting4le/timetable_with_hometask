d9c783e4cc7b1ab32d319756a54ddfc50a4d2867
N1ghting4le <nikansoloveib@gmail.com>
2023-11-14 15:24:43 +0300

    version 1.0.0

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,16 +1,12 @@
 const useHttp = () => {
     const request = async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}, cache = "no-store") => {
-        try {
-            const res = await fetch(url, { method, headers, body, cache });
+        const res = await fetch(url, { method, headers, body, cache });
 
-            if (!res.ok) {
-                throw new Error(`Failed to fetch ${url}, status: ${res.status}`);
-            }
-
-            return res.json();
-        } catch (e) {
-            console.error(e);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch ${url}, status: ${res.status}`);
         }
+
+        return res.json();
     }
 
     return request;
