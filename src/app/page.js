6fc25daf6dc9be0ab5d1b481dc2f068e43d546a5
N1ghting4le <nikansoloveib@gmail.com>
2024-01-03 12:28:46 +0300

    version 1.1.2 tynkovich changes now

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import WeekList from '@/components/weekList/WeekList';
 import styles from './page.module.css';
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main className={styles.main}>
